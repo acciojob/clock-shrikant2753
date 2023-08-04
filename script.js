084adf245a4a3b9bfe3a908dfa1150f2a486e160
Shrikant Rajendra Patil <58933004+shrikant2753@users.noreply.github.com>
2023-08-04 17:55:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+function updateTimer() {
+            const timerElement = document.getElementById("timer");
+            const now = new Date();
+
+            const dateOptions = { day: 'numeric', month: 'numeric', year: 'numeric' };
+            const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+
+            const dateStr = now.toLocaleDateString(undefined, dateOptions);
+            const timeStr = now.toLocaleTimeString(undefined, timeOptions);
+
+            timerElement.textContent = `${dateStr}, ${timeStr}`;
+        }
+
+        // Initial update
+        updateTimer();
+
+        // Update the timer every second (1000 milliseconds)
+        setInterval(updateTimer, 1000);
